a7eb7dbbae5448b50764bdfb50caa975d2a22835
midastop <midastop@naver.com>
2025-07-17 18:12:27 +0900

    springboot-chat01 프로젝트 생성

---
+++ springboot-chat01/src/main/java/com/springbootstudy/chat/SpringbootChat01Application.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChat01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChat01Application.class, args);
+	}
+
+}
---
+++ springboot-chat01/src/test/java/com/springbootstudy/chat/SpringbootChat01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChat01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
